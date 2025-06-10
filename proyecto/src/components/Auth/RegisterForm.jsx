3fd438401768351eb0eb43c7a88e59b855f6efc9
Juan Tornay <120737352+juan-Tornay@users.noreply.github.com>
2025-06-10 21:00:42 +0200

    actualizacion

--- proyecto/src/components/Auth/RegisterForm.jsx
+++ proyecto/src/components/Auth/RegisterForm.jsx
@@ -1,9 +1,7 @@
-
 import { registerUser, saveUserToLocal, getUserFromLocal } from '../services/auth_API';
 
 import React, { useState, useEffect } from 'react';
 import '../styles/register.css';
-import { saveUserToLocal, getUserFromLocal } from '../services/auth_API';
 
 import { validateEmail, validatePassword, checkDuplicateUser } from '../Shared/ValidationSystem';
 import axios from 'axios';
--- proyecto/src/components/services/auth_API.js
+++ proyecto/src/components/services/auth_API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://tfg-7.onrender.com";
 
 export const saveUserToLocal = (user) => {
   localStorage.setItem('registeredUser', JSON.stringify(user));
